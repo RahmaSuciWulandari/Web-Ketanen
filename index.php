35884c17989a37611e3fc58295778a9fbe5a0e3e
Suci <rahmawul504@gmail.com>
2025-01-10 08:25:56 +0700

    update

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   </title>
   <script src="https://cdn.tailwindcss.com">
   </script>
-  <!-- <link rel="stylesheet" href="style.css" type="text/css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/> -->
+  <!-- <link rel="stylesheet" href="style.css" type="text/css">-->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/> 
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet"/>
  </head>
  <body class="font-roboto bg-gray-100">
